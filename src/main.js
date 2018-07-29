a6b97cced7321bd9d6cf60cc25bf6c50bedeeca1
mokuo <tennis10988.yk@gmail.com>
2018-07-29 21:02:36 +0900

    p85 $elの使い方

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 var app = new Vue({
   el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 }
-    ]
-  },
-  methods: {
-    doAttack: function(index) {
-      this.list[index].hp -= 10
-    }
+  mounted: function() {
+    console.log(this.$el);
   }
 })
